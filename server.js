2bb5c9d704190240992e4dd05d203b50b47d9e31
ghelpo <m.f.ragab5890@gmail.com>
2020-10-04 02:36:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.static('website'));
 
 // Setup Server
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 80;
 
 listening = () => {
   console.log("running server on port "+ port);
